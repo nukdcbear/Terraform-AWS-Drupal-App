b86192d35cad02aeedb282266d572076aad2c3e7
David Barringer <davidcbarringer@gmail.com>
2020-05-21 12:21:09 -0400

    Update to run in TFE

--- terraform.tf
+++ terraform.tf
@@ -3,13 +3,13 @@ terraform {
   required_providers {
     aws = "~> 2.62"
   }
-  backend "s3" {
-    bucket  = "dcb-tfe-bootcamp-tfstate"
-    key     = "tfstates/pu-drupal-app"
-    region  = "us-east-2"
-    encrypt = true
-  }
-}
+#   backend "s3" {
+#     bucket  = "dcb-tfe-bootcamp-tfstate"
+#     key     = "tfstates/pu-drupal-app"
+#     region  = "us-east-2"
+#     encrypt = true
+#   }
+# }
 
 provider "aws" {
   region  = "us-west-1"
